6050eba039eb96d9b0fa654074049c746fdf9e54
zachariyaJaveri <35935423+zachariyaJaveri@users.noreply.github.com>
2023-04-06 21:04:59 -0400

    finding another way to fail the test

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -14,8 +14,7 @@ public class ArithmeticOperationsTest {
 		ArithmeticOperations operations = new ArithmeticOperations();
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
-		//assertEquals(expected, actual);
-		assertNotNull(null);
+		assertEquals(expected, 0);
 	}
     //@Test
     //public void testSub()
